a91973f25d4f7299c65573d879cd6aa5d723a0c4
Jason Law <futbol10@users.noreply.github.com>
2022-04-28 23:33:05 -0400

    updated to a post request and exporting as excel. started on form parameters to query

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,33 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from google.cloud import bigquery
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/jason/Documents/analyzequicker-bigquery-sa.json'
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/query",methods = ['POST'])
 def hello_world():
     client = bigquery.Client()
+
+    # form = request.form
+    # parameters = {}
+    # for key in form.keys():
+    #     parameters[key] = form[key]
+
+    # return parameters
+
+    limit = request.form['limit']
+
     query = """
         SELECT subject AS subject, COUNT(*) AS num_duplicates
         FROM bigquery-public-data.github_repos.commits
         GROUP BY subject 
         ORDER BY num_duplicates 
-        DESC LIMIT 10 
-    """
+        DESC LIMIT {limit} 
+    """.format(limit=limit)
 
-    # return client.query(query).to_dataframe().to_html(index=False)
+    # return client.query(query).to_dataframe().to_json()
 
-    client.query(query).to_dataframe().to_csv('test.csv',index=False)
+    client.query(query).to_dataframe().to_excel('test.xlsx',sheet_name='Sheet1',index=False)
 
-    return send_file('test.csv', mimetype='text/csv')
-    
-    
+    return send_file('test.xlsx', mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
